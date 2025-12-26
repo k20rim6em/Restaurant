fdd7be9d356e58633d228cf0d78caef530df7efb
Mero-297 <s-mariem.yousef@zewailcity.edu.eg>
2025-12-26 15:45:18 +0200

    Update Cook.cpp

--- CIE205_/Restaurant/Rest/Cook.cpp
+++ CIE205_/Restaurant/Rest/Cook.cpp
@@ -1,30 +1,33 @@
 #include "Cook.h"
 
-
-Cook::Cook(int id, ORD_TYPE ctype, int cspeed) 
-{ 
+Cook::Cook(int id, ORD_TYPE ctype, int cspeed)
+{
 	ID = id;
 	type = ctype;
 	speed = cspeed;
-}
 
+	isBusy = false;
+	inBreak = false;
+	currentOrder = nullptr;
+	ordersDone = 0;
+
+	breakDuration = 0;
+	breakStartTime = -1;
+}
 
 Cook::~Cook()
 {
 }
-
 
 int Cook::GetID() const
 {
 	return ID;
 }
 
-
 ORD_TYPE Cook::GetType() const
 {
 	return type;
 }
-
 
 void Cook::setID(int id)
 {
@@ -45,3 +48,62 @@ void Cook::SetSpeed(int s)
 {
 	speed = s;
 }
+
+// ================= States =================
+
+bool Cook::IsBusy() const
+{
+	return isBusy;
+}
+
+bool Cook::IsInBreak() const
+{
+	return inBreak;
+}
+
+bool Cook::CanTakeOrder() const
+{
+	return !isBusy && !inBreak;
+}
+
+// ================= Orders =================
+
+void Cook::AssignOrder(Order* ord)
+{
+	currentOrder = ord;
+	isBusy = true;
+}
+
+void Cook::FinishOrder()
+{
+	currentOrder = nullptr;
+	isBusy = false;
+	ordersDone++;
+}
+
+int Cook::GetOrdersDone() const
+{
+	return ordersDone;
+}
+
+
+
+void Cook::SetBreakDuration(int duration)
+{
+	breakDuration = duration;
+}
+
+void Cook::StartBreak(int timestep)
+{
+	inBreak = true;
+	isBusy = true;          
+	breakStartTime = timestep;
+}
+
+void Cook::EndBreak()
+{
+	inBreak = false;
+	isBusy = false;
+	breakStartTime = -1;
+}
+
